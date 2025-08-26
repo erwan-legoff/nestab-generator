94e2f4184e7dd942b359e2232d0cad50220ac2e6
Erwan Le Goff <rwan.legoff@gmail.com>
2025-08-26 12:50:02 +0200

    better generator used

--- src/melodies/melodies.service.ts
+++ src/melodies/melodies.service.ts
@@ -2,11 +2,13 @@ import { Injectable } from '@nestjs/common';
 import { SimpleDownArpegeCompositor } from 'src/compositor/SimpleDownArpegeCompositor';
 import { GenerateMelodyDto } from './dto/generate-melody.dto';
 import NoteOne from 'src/notes/NoteOne';
+import { CompositorInterface } from 'src/compositor/CompositorInterface';
+import { RandomSimpleCompositor } from 'src/compositor/RandomSimpleCompositor';
 
 @Injectable()
 export class MelodiesService {
   previewGenerate(generateMelodyDto: GenerateMelodyDto) {
-    const compositor = new SimpleDownArpegeCompositor();
+    const compositor : CompositorInterface = new RandomSimpleCompositor()
     return compositor.getMusic(
       generateMelodyDto.notesCount,
       generateMelodyDto.tempo,
