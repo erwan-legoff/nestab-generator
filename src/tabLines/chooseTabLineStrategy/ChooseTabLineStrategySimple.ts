b0a8ab2c90c264777d9a93f8785ba37302aa1ad6
Erwan Le Goff <rwan.legoff@gmail.com>
2025-08-26 17:34:11 +0200

    fixing inappropriate behavior for a better tab generator

--- src/tabLines/chooseTabLineStrategy/ChooseTabLineStrategySimple.ts
+++ src/tabLines/chooseTabLineStrategy/ChooseTabLineStrategySimple.ts
@@ -1,7 +1,7 @@
-import { NoteOneInterface } from '../../notes/NoteOneInterface'
-import { NoteOneSilence } from '../../notes/NoteOneSilence'
-import TabLine from '../TabLine'
-import { ChooseTabLineStrategy } from './ChooseTabLineStrategy'
+import { NoteOneInterface } from '../../notes/NoteOneInterface';
+import { NoteOneSilence } from '../../notes/NoteOneSilence';
+import TabLine from '../TabLine';
+import { ChooseTabLineStrategy } from './ChooseTabLineStrategy';
 
 export class ChooseTabLineStrategySimple implements ChooseTabLineStrategy {
   /**
@@ -11,48 +11,48 @@ export class ChooseTabLineStrategySimple implements ChooseTabLineStrategy {
    */
   chooseTabLine(note: NoteOneInterface, tabLines: TabLine[]): number {
     //choose the right string number according to the note to play : the string root note should be lower than the note and also the closest note
-    if (note instanceof NoteOneSilence) return -1
+    if (note instanceof NoteOneSilence) return -1;
 
-    const midi = note.getMidi()
-    let difference = tabLines[0].getRoot().getMidi() - midi
-    let choosenIndex: number = 0
+    const midi = note.getMidi();
+    let minDifference = midi - tabLines[0].getRoot().getMidi();
+    let chosenIndex: number = 0;
 
     for (let i = 0; i < tabLines.length; i++) {
-      const tabLine = tabLines[i]
+      const tabLine = tabLines[i];
       if (tabLine.getRoot().getMidi() === midi) {
-        return i
+        return i;
       }
     }
 
-    choosenIndex = 0
+    chosenIndex = 0;
     for (let stringNumber = 0; stringNumber < tabLines.length; stringNumber++) {
-      const tabLine = tabLines[stringNumber]
-      const tabLineRoot = tabLine.getRoot().getMidi()
-      const currentDifference = tabLineRoot - midi
+      const tabLine = tabLines[stringNumber];
+      const tabLineRoot = tabLine.getRoot().getMidi();
+      const currentDifference = midi - tabLineRoot;
       if (
         currentDifference > 0 &&
-        (difference >= currentDifference || difference < 0)
+        (currentDifference <= minDifference || minDifference < 0)
       ) {
-        difference = currentDifference
-        choosenIndex = stringNumber
+        minDifference = currentDifference;
+        chosenIndex = stringNumber;
       }
     }
 
-    if (difference > 0) return choosenIndex
+    if (minDifference > 0) return chosenIndex;
 
     // If no lower tabLine is found, we choose the closest tabLine even if the root is higher than the note
-    difference = Math.abs(difference)
-    choosenIndex = 0
+    minDifference = Math.abs(minDifference);
+    chosenIndex = 0;
     for (let stringNumber = 0; stringNumber < tabLines.length; stringNumber++) {
-      const tabLine = tabLines[stringNumber]
-      const tabLineRoot = tabLine.getRoot().getMidi()
-      const currentDifference = Math.abs(tabLineRoot - midi)
-      if (difference <= currentDifference) {
-        difference = currentDifference
-        choosenIndex = stringNumber
+      const tabLine = tabLines[stringNumber];
+      const tabLineRoot = tabLine.getRoot().getMidi();
+      const currentDifference = Math.abs(midi - tabLineRoot);
+      if (currentDifference <= minDifference) {
+        minDifference = currentDifference;
+        chosenIndex = stringNumber;
       }
     }
 
-    return choosenIndex
+    return chosenIndex;
   }
 }
